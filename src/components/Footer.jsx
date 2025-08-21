d85969215d38f62911f54fb473edb0cd61753d26
Shadd <schutte2@msn.com>
2025-08-21 13:18:58 -0600

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = ()=>{
-    return(
-        <footer className="px-6 py-4 text-center text-white bg-gray-600">
-            <p>&copy; All rights reserved to myapp.com</p>
-        </footer>
-    );
-};
-
-export { Footer };
